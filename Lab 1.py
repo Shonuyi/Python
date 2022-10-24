ee3a5c87c77940b481de9030b260dbe96645d336
Shonuyi <47195919+Shonuyi@users.noreply.github.com>
2022-10-24 11:41:54 -0400

    Calculate Flooring

---
+++ Lab 1.py
@@ -0,0 +1,47 @@
+
+# Program Name: Lab1.py (Lab 1)
+# Course: IT1114/Section W03
+# Student Name: TITILAYO SHONUYI
+# Assignment Number: Lab1 
+# Due Date: 08/28/ 2022
+# Purpose: Help users to Calculate the Total Cost of Installing Flooring
+# w3schools.com/python/python_user_input.asp and pythonguides.com
+
+
+Length1= float(input(" Enter length of the room "))             #getting user input
+Width1 = float(input(" Enter width of the room "))              #getting user input
+Cost_sqft= float(input(" Enter cost of flooring per sqft "))    #getting user input
+
+Measurement = (Length1 * Width1)                                # calculating sqft footage
+
+Sub_Total = Measurement * Cost_sqft                             #calculating Subtotal of flooring
+
+Sales_tax = 0.07                                                # Sales tax
+
+Tax = Sub_Total * Sales_tax
+
+Total_Cost = Sub_Total + Tax                                    #The price the user has to pay
+
+Total_Cost1 = "{:.2f}".format(Total_Cost)                       #Eliminate unnecessary decimal
+
+print()
+print()
+print("Flooring Dimensions: ")
+
+print()
+print("\t" + " Room Length: " + str(Length1))
+print()
+print("\t" + " Room Width:  " + str(Width1))
+
+print()
+print()
+print("Flooring Cost: ")
+print()
+print("\t" + " Square feet: " + str(Length1))
+print()
+print("\t" + " Flooring (" + str(Cost_sqft) + "/sq.ft) " + "$" + str(Sub_Total))
+print()
+print("\t" + " Tax (7%)" + "              $" + str(Tax))
+print()
+
+print("\t" + " Total" + "                 $" + (Total_Cost1))
